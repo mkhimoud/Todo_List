d1d91bd40f85ed356607b02e0827663d845a61f8
Massinissa <khimoudmassinissa@gmail.com>
2022-12-16 23:09:56 +0100

    message

--- main.js
+++ main.js
@@ -1,75 +1,33 @@
-showtask();
-let addlist = document.getElementById("addlist");
-let addtbutton = document.getElementById("addtbutton");
+let response = document.getElementById('response');
+let data_name = document.getElementById('data_name');
+let valider = document.getElementById('valider');
+let clear = document.getElementById('clear');
 
-const todoList = document.querySelector(".todoList");
+var localArr = [];
+var tabUsers = [];
 
-addtbutton.addEventListener("click", function(){
-    addlistval = addlist.value;
-    if(addlistval.trim()!=0){
-        let webtask = localStorage.getItem("localtask");
-        if(webtask == null){
-            taskObj = [];
-        }
-        else{
-            taskObj = JSON.parse(webtask);
-        }
-        taskObj.push({'task_name':addlistval, 'completeStatus':false});
-		// console.log(taskObj, 'Ashendra');
-        localStorage.setItem("localtask", JSON.stringify(taskObj));
-        addlist.value = '';
-    }
-    showtask();
-})
- 
-function showtask(){
-    let webtask = localStorage.getItem("localtask");
-    if(webtask == null){
-        taskObj = [];
-    }
-    else{
-        taskObj = JSON.parse(webtask);
-    }
-    let html = '';
-    let addedtasklist = document.getElementById("addedtasklist");
-    taskObj.forEach((item, index) => {
-
-        if(item.completeStatus==true){
-            taskCompleteValue = `<li class="completed">${item.task_name}</li>`;
-        }else{
-            taskCompleteValue = `<li>${item.task_name}</li>`;
-        }
-        html += `<li>                
-        ${taskCompleteValue}
-         <li> <button type="button" onclick="deleteitem(${index})" class="button_supprimer"><i class="fas fa-trash"></i></li>
-         <li>` ;
+if (localStorage.getItem('users') != null) {
+    localArr = localStorage.getItem('users').split(',');
+    localArr.forEach(function(item, index) {
+        response.innerHTML += "<p> -  " + item + " </p> ";
+        console.log(item)
     });
-    addedtasklist.innerHTML = html;
 }
 
+valider.addEventListener('click', function() {
+    tabUsers.push(data_name.value);
+    let tabString = tabUsers.toString();
+    localStorage.setItem('users', tabString);
+    localArr = localStorage.getItem('users').split(',');
+    console.log(localArr);
+    localArr.forEach(function(item, index) {
+        response.innerHTML +=  "<p> - " + item + " </p> ";"<p>" + item + "</p>";       
+    });
+});
 
-function deleteitem(index){
-    let webtask = localStorage.getItem("localtask");
-    let taskObj = JSON.parse(webtask);
-    taskObj.splice(index, 1);
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
-    showtask();
-}
+clear.addEventListener('click', function() {
+    localStorage.clear();
+});
 
-
-let deleteall = document.getElementById("deleteall");
-deleteall.addEventListener("click", function(){   
-    let webtask = localStorage.getItem("localtask");
-    let taskObj = JSON.parse(webtask);
-    if(webtask == null){
-        taskObj = [];
-    }
-    else{
-        taskObj = JSON.parse(webtask);
-        taskObj = [];
-    }
-    localStorage.setItem("localtask", JSON.stringify(taskObj));
-    showtask();
-})
 
 
